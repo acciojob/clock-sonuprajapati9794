244650427488307c46941cd4ed66d5c93878d6cb
sonuprajapati9794 <prajapati979473@gmail.com>
2026-04-16 22:53:45 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,18 @@
-//your JS code here. If required.
+function updateTimer() {
+    let now = new Date(); // current date & time
+
+    // format date
+    let date = now.toLocaleDateString();
+
+    // format time
+    let time = now.toLocaleTimeString();
+
+    // show in p tag
+    document.getElementById("timer").textContent = date + " " + time;
+}
+
+// run every second
+setInterval(updateTimer, 1000);
+
+// call once immediately (important)
+updateTimer();
