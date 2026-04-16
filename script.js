6bf614186ebfd77a41321d449146446529634dd4
sonuprajapati9794 <prajapati979473@gmail.com>
2026-04-16 23:11:21 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,10 +1,6 @@
-let timerDisplay=document.getElementById("timer");
-let currentDate=new Date();
-setInterval(
-	function(){
-		currentDate=new Date();
-		timmerDisplay.innerHTML=currentDate.toLocaleString();
-		
-		
-	},1000
-)
+let timerDisplay = document.getElementById("timer");
+
+setInterval(function () {
+    let currentDate = new Date(); // inside function
+    timerDisplay.textContent = currentDate.toLocaleString();
+}, 1000);
